4283d7b7659ba5676875b9f7cb6cfa2a91f17e5e
Paulo <Pauloitalo@hotmail.com.br>
2017-05-16 18:42:58 -0300

    insercao dos regexes de cada pagina mais concerto de algumas paginas

--- modelo/views.py
+++ modelo/views.py
@@ -24,11 +24,91 @@ def contato(request):
 def login(request):
     return render(request, 'view/login.html')
 
+def index_cliente(request):
+    return render(request, 'view/indexC.html')
+
+def menu_cliente(request):
+    return render(request, 'view/cardapioC.html')
+
+def sobre_cliente(request):
+    return render(request, 'view/sobreNosC.html')
+
+def contato_cliente(request):
+    return render(request, 'view/contatoC.html')
+
+def perfil_cliente(request):
+    return render(request, 'view/perfil.html')
+
+def consultar_cliente(request):
+    return render(request, 'view/ConsultarCF.html')
+
+def index_funcionario(request):
+    return render(request, 'view/indexF.html')
+
+def menu_funcionario(request):
+    return  render(request, 'view/cardapioF.html')
+
+def sobre_funcionario(request):
+    return  render(request, 'view/sobreNosF.html')
+
+def contato_funcionario(request):
+    return render(request, 'view/contatoF.html')
+
+def vendas_funcionario(request):
+    return render(request, 'view/venda.html')
+
 def delete_estoque(request,id):
 
     estoque = Estoque.objects.filter(id=id)
     estoque.delete()
     return HttpResponseRedirect("/consulta/")
+
+def editar_funcionario(request):
+    return render(request, 'view/editeEdicaoF.html')
+
+def new_cliente(request):
+    return render(request, 'view/cadF.html')
+
+def editar_cliente_funcionario(request):
+    return render(request, 'view/editeEdicaoF.html')
+
+def index_gerente(request):
+    return render(request, 'view/indexG.html')
+
+def menu_gerente(request):
+    return render(request, 'view/cardapioG.html')
+
+def sobre_gerente(request):
+    return render(request, 'view/sobreNosG.html')
+
+def contato_gerente(request):
+    return render(request, 'view/contatoG.html')
+
+def new_funcionario(request,id):
+    data = {}
+
+    if request.method == "POST":
+        data['cpf'] = request.POST.get('cpf')
+    else:
+        form = FuncionarioForm()
+    data['form'] = form
+    return render(request, 'view/cadG.html', data)
+
+def consultar_funcionario(request):
+
+    data = {}
+    data['funcionarios'] = Funcionario.objects.all()
+    data['usuarios'] = Usuario.objects.all()
+    return render(request, 'view/ConsultarF.html', data)
+
+def editar_funcionario_gerente(request):
+    return render(request, 'view/editeEdicaoG.html')
+
+def consultar_historico(request):
+    return render(request, 'view/consultarH.html')
+
+def vendas_gerente(request):
+    return render(request, 'view/vendaG.html')
 
 def consulta_estoque(request):
     data = {}
@@ -52,22 +132,6 @@ def edita_estoque(request,pk):
 
     return render(request, 'view/editeEdicaoG.html', data)
 
-def consulta_func(request):
-
-    data = {}
-    data['funcionarios'] = Funcionario.objects.all()
-    data['usuarios'] = Usuario.objects.all()
-    return render(request, 'view/ConsultarF.html', data)
-
-def new_func(request,id):
-    data = {}
-
-    if request.method == "POST":
-        data['cpf'] = request.POST.get('cpf')
-    else:
-        form = FuncionarioForm()
-    data['form'] = form
-    return render(request, 'view/cadG.html', data)
 
 def new_estoque(request):
     data = {}
